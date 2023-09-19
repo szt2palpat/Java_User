e4e5617f87cd2494d3f6954c325e74bf8099860e
szt2palpat <szt2palpat@vasvari.org>
2023-09-19 13:00:10 +0200

    Dun

---
+++ src/HelloUser.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        String name;
+        Scanner n=new Scanner(System.in);
+        System.out.println("Mi a neved?");
+        name=n.nextLine();
+                System.out.printf("Hello, %s!", name);
+            }
+        }
